3bae94738de3e195060dea22c1591db4ff4ba307
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-03 09:44:42 -0700

    update function for fourth spec to convert numbers to strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 //Business Logic
+function countArray(number) {
+  var numbers = [];
 
+  for (var i = 0; i < number + 1; i++) {
+    var numberString = i.toString();
+    numbers.push(numberString);
+  }
+  return numbers;
+}
 
 
 //User Interface Logic
@@ -8,9 +16,11 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userInput = parseInt($("#number").val());
+
+    var numberArray = countArray(userInput);
     
-    console.log(typeof(userInput));
-    $("#results").html("<li>" + userInput + "</li>");
+    console.log(typeof(numberArray[1]));
+    $("#results").html("<li>" + numberArray + "</li>");
     $(".results").show();
   })
 })
